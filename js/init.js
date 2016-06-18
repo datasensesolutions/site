e780ba8bd4f08c2d3475a6d814e162dc1d9f4c44
joshardul <joshardul@users.noreply.github.com>
2016-06-18 19:06:29 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -10,6 +10,7 @@
         $('.modal-trigger').leanModal();
         $('ul.tabs').tabs();
         $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
+        $('select').material_select();
         $(document).ready(function () {
             Materialize.updateTextFields();
         });
