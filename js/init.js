b95702c229a984d90cbfe3221d6eed2d7a455c14
joshardul <joshardul@users.noreply.github.com>
2016-06-20 19:52:31 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -11,6 +11,10 @@
         $('ul.tabs').tabs();
         $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset().top });
         $('select').material_select();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
         $(document).ready(function () {
             Materialize.updateTextFields();
         });
